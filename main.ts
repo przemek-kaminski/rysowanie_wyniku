3daeebf3bca5b94661881bc578c5d1695092dc3b
przemek-kaminski <88503183+przemek-kaminski@users.noreply.github.com>
2021-08-05 18:31:52 +0200

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,21 @@
+function wyświetl_diody (ile_diod: number) {
+    ile_wyświetliliśmy = 0
+    for (let kolumna = 0; kolumna <= 2; kolumna++) {
+        for (let wiersz = 0; wiersz <= 4; wiersz++) {
+            if (ile_wyświetliliśmy < ile_diod) {
+                led.plot(kolumna + 1, wiersz)
+                ile_wyświetliliśmy += 1
+            } else {
+                led.unplot(kolumna + 1, wiersz)
+            }
+        }
+    }
+}
+let ile_wyświetliliśmy = 0
+while (true) {
+    wyświetl_diody(randint(0, 15))
+    basic.pause(500)
+}
+basic.forever(function () {
+	
+})
